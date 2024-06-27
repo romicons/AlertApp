d7294b72085ab4ba920036a724ca9cb2fcfe5152
romicons <romina.constantinoff@gmail.com>
2024-06-26 21:19:10 -0300

    Started to work on the notifications panel.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,21 @@ import AlertAppIcon from "../assets/AlertAppIcon.svg"
 
 export const Navbar = () => {
     return (
-        <Box sx={ { display:"flex", justifyContent:"left", flexFlow: "row wrap", alignItems:"center", backgroundColor: "#f9f7ff", width:'100%', paddingInline: 2,  boxShadow: 3, gap: 2 } }>
-            <Box component="img" sx={{
-                width: 35,
-                height: 'auto', 
-            }}
-            alt="AlertApp Logo"
-            src={AlertAppIcon}
-            >        
+        <Box sx={ { display:"flex", justifyContent:"space-between", flexFlow: "row wrap", alignItems:"center", backgroundColor: "#f9f7ff", width:'100%', paddingInline: 2,  boxShadow: 3 } }>
+            <Box sx={ { display:"flex", gap: 2, alignItems: "center"}}>
+                <Box component="img" sx={{
+                    width: 35,
+                    height: 'auto', 
+                }}
+                alt="AlertApp Logo"
+                src={AlertAppIcon}
+                >
+                </Box>
+                <Typography variant="h1" sx={{ fontSize:40}}>AlertApp</Typography>
             </Box>
-            <Typography variant="h1" sx={{ fontSize:40}}>AlertApp</Typography>
+            <Typography variant="h3" sx={{ fontSize:20}}>Notificaciones
+
+            </Typography>
         </Box>
     )
 }
